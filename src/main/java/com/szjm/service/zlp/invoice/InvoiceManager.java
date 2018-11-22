fc82af1b38349f2e5bd1e010c9803d5ce1dd0b56
myaslp <1322044350@qq.com>
2018-11-22 16:36:27 +0800

    订单发票添加

---
+++ src/main/java/com/szjm/service/zlp/invoice/InvoiceManager.java
@@ -0,0 +1,58 @@
+package com.szjm.service.zlp.invoice;
+
+import java.util.List;
+import com.szjm.entity.Page;
+import com.szjm.util.PageData;
+
+/** 
+ * 说明： 订单发票接口
+ * 创建人：
+ * 创建时间：2018-11-22
+ * @version
+ */
+public interface InvoiceManager{
+
+	/**新增
+	 * @param pd
+	 * @throws Exception
+	 */
+	public void save(PageData pd)throws Exception;
+	
+	/**删除
+	 * @param pd
+	 * @throws Exception
+	 */
+	public void delete(PageData pd)throws Exception;
+	
+	/**修改
+	 * @param pd
+	 * @throws Exception
+	 */
+	public void edit(PageData pd)throws Exception;
+	
+	/**列表
+	 * @param page
+	 * @throws Exception
+	 */
+	public List<PageData> list(Page page)throws Exception;
+	
+	/**列表(全部)
+	 * @param pd
+	 * @throws Exception
+	 */
+	public List<PageData> listAll(PageData pd)throws Exception;
+	
+	/**通过id获取数据
+	 * @param pd
+	 * @throws Exception
+	 */
+	public PageData findById(PageData pd)throws Exception;
+	
+	/**批量删除
+	 * @param ArrayDATA_IDS
+	 * @throws Exception
+	 */
+	public void deleteAll(String[] ArrayDATA_IDS)throws Exception;
+	
+}
+
---
+++ src/main/java/com/szjm/service/zlp/invoice/impl/InvoiceService.java
@@ -0,0 +1,82 @@
+package com.szjm.service.zlp.invoice.impl;
+
+import java.util.List;
+import javax.annotation.Resource;
+import org.springframework.stereotype.Service;
+import com.szjm.dao.DaoSupport;
+import com.szjm.entity.Page;
+import com.szjm.util.PageData;
+import com.szjm.service.zlp.invoice.InvoiceManager;
+
+/** 
+ * 说明： 订单发票
+ * 创建人：
+ * 创建时间：2018-11-22
+ * @version
+ */
+@Service("invoiceService")
+public class InvoiceService implements InvoiceManager{
+
+	@Resource(name = "daoSupport")
+	private DaoSupport dao;
+	
+	/**新增
+	 * @param pd
+	 * @throws Exception
+	 */
+	public void save(PageData pd)throws Exception{
+		dao.save("InvoiceMapper.save", pd);
+	}
+	
+	/**删除
+	 * @param pd
+	 * @throws Exception
+	 */
+	public void delete(PageData pd)throws Exception{
+		dao.delete("InvoiceMapper.delete", pd);
+	}
+	
+	/**修改
+	 * @param pd
+	 * @throws Exception
+	 */
+	public void edit(PageData pd)throws Exception{
+		dao.update("InvoiceMapper.edit", pd);
+	}
+	
+	/**列表
+	 * @param page
+	 * @throws Exception
+	 */
+	@SuppressWarnings("unchecked")
+	public List<PageData> list(Page page)throws Exception{
+		return (List<PageData>)dao.findForList("InvoiceMapper.datalistPage", page);
+	}
+	
+	/**列表(全部)
+	 * @param pd
+	 * @throws Exception
+	 */
+	@SuppressWarnings("unchecked")
+	public List<PageData> listAll(PageData pd)throws Exception{
+		return (List<PageData>)dao.findForList("InvoiceMapper.listAll", pd);
+	}
+	
+	/**通过id获取数据
+	 * @param pd
+	 * @throws Exception
+	 */
+	public PageData findById(PageData pd)throws Exception{
+		return (PageData)dao.findForObject("InvoiceMapper.findById", pd);
+	}
+	
+	/**批量删除
+	 * @param ArrayDATA_IDS
+	 * @throws Exception
+	 */
+	public void deleteAll(String[] ArrayDATA_IDS)throws Exception{
+		dao.delete("InvoiceMapper.deleteAll", ArrayDATA_IDS);
+	}
+	
+}
+
